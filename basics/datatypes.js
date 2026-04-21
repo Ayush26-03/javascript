77202123cb9e4f05d916038976a51b6f00ae80e0
Error404 <ayushp4062@gmail.com>
2026-04-21 07:04:51 +0000

    datatypes,variables

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as new version
+
+//alert(3+3)// we are using nodejs,not browser
+
+let name ="hitesh"//String
+let age=18//Int
+let isLoggedIn = false// Boolean
+
+//num
+//bigint
+// string
+//boolean=> true/false
+//null =>standalone value
+//undefined=>not assigned
+//symbol=>i=unique
+
+
+//object
+
+console.log(typeof "hitesh")//gives the type of data
---
+++ basics/variable.js
@@ -0,0 +1,11 @@
+const accountId=14453
+let accountemail="xyz@google.com"
+var accountPassword="123456"
+accountcity="jaipur"
+
+//accountId=22 not allowed
+ccountemail="zyx@google.com"
+accountPassword="2468"
+accountcity="bengluru"
+
+console.table([accountId,accountemail,accountPassword,accountcity]);
